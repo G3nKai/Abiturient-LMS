2512ae11b50ea20c9ccab29c4e9ecbf726628636
Ileeya <164293141+G3nKai@users.noreply.github.com>
2026-08-27 13:18:40 +0700

    Add project files.

---
+++ Abiturient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Abiturient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

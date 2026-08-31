990bc5a5dfd3d1936c1ffa93d44f7a8c6814b24b
Ileeya <164293141+G3nKai@users.noreply.github.com>
2026-08-31 11:42:35 +0700

    deleting everything

--- Abiturient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Abiturient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Common/Enum/Gender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Applicant.Data.Models.Enum
-{
-    public enum Gender
-    {
-        Male,
-        Female
-    }
-}
